cf6e4f6ec2f3e55f012fa983748092adb6554b49
biWenZhe <2368688782@qq.com>
2022-09-21 22:17:31 +0800

    更新结构

---
+++ src/main/java/com/news/Tool/SqlLink.java
@@ -0,0 +1,24 @@
+package com.news.Tool;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlLink {
+    private static SqlSessionFactory build;
+    static {
+        try {
+            String re = "mybatis-config.xml";
+            InputStream resourceAsStream = Resources.getResourceAsStream(re);
+            build = new SqlSessionFactoryBuilder().build(resourceAsStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static SqlSessionFactory getSqlSessionFactory(){
+        return build;
+    }
+}
